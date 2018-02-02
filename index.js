fa36a55cef58eddfbb11d1a8ae9fffcb7a531313
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-02-02 13:22:14 +0900

    Enable translation

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ server.listen(process.env.PORT || 5000, () => {
 server.use("/webhook", bot_express({
     nlu: {
         type: "dialogflow",
-        language: "ja",
         options: {
             client_access_token: process.env.DIALOGFLOW_CLIENT_ACCESS_TOKEN
         }
@@ -28,14 +27,12 @@ server.use("/webhook", bot_express({
         type: "memory-cache",
         retention: 600
     },
+    language: "ja",
     line_channel_secret: process.env.LINE_CHANNEL_SECRET,
     line_access_token: process.env.LINE_ACCESS_TOKEN,
-    facebook_app_secret: process.env.FACEBOOK_APP_SECRET,
-    facebook_page_access_token: [
-        {page_id: process.env.FACEBOOK_PAGE_ID, page_access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN}
-    ],
     default_skill: process.env.DEFAULT_SKILL,
     google_project_id: process.env.GOOGLE_PROJECT_ID,
+    google_api_key: process.env.GOOGLE_API_KEY,
     auto_translation: process.env.AUTO_TRANSLATION
 }));
 
